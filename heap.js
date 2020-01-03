8510ee2243b3a7fd9c75cda8b7744f4f75c9828d
滕怡君 <1057120672@qq.com>
2020-01-03 16:11:44 +0800

    minHeap

--- heap.js
+++ heap.js
@@ -1,4 +1,125 @@
 /**
+ * 最小堆
+ * 2020/1/3
+ */
+class MinHeap {
+  constructor() {
+    this.heap = [null]
+  }
+
+  //插入
+  insert(val) {
+    let i = this.heap.length,
+      p = Math.floor(i / 2)
+    this.heap.push(val)
+
+    while (i > 1) {
+      if (this.heap[i] < this.heap[p]) {
+        ;[this.heap[i], this.heap[p]] = [this.heap[p], this.heap[i]]
+        i = p
+        p = Math.floor(i / 2)
+      } else {
+        break
+      }
+    }
+  }
+
+  //删除
+  del(val) {
+    let i = this.heap.indexOf(val)
+    if (i === -1) return
+
+    //要删除的节点与最后一个节点交换位置
+    ;[this.heap[i], this.heap[this.heap.length - 1]] = [
+      this.heap[this.heap.length - 1],
+      this.heap[i]
+    ]
+    //删除最后一个节点
+    this.heap.pop()
+
+    //维持小顶堆
+    while (2 * i <= this.heap.length) {
+      let cur = this.heap[i],
+        right = this.heap[2 * i + 1],
+        left = this.heap[2 * i]
+
+      if (cur <= left && (!right || cur <= right)) return
+      if (left < cur && (!right || left < right)) {
+        ;[this.heap[i], this.heap[2 * i]] = [this.heap[2 * i], this.heap[i]]
+        i = i * 2
+      } else if (right && right < cur && right < left) {
+        ;[this.heap[i], this.heap[2 * i + 1]] = [
+          this.heap[2 * i + 1],
+          this.heap[i]
+        ]
+        i = i * 2 + 1
+      }
+    }
+  }
+
+  //排序（降序）
+  sort() {
+    if (this.heap.length < 3) return
+
+    for (let i = 1, len = this.heap.length; i < len; i++) {
+      //第一个和倒数第i个数交换位置
+      ;[this.heap[1], this.heap[len - i]] = [this.heap[len - i], this.heap[1]]
+
+      //维持倒数第i个数之前的所有数为小顶堆
+      let f = 1
+      while (2 * f <= len - 1 - i) {
+        let cur = this.heap[f],
+          left = this.heap[f * 2],
+          right = this.heap[f * 2 + 1]
+
+        if (cur <= left && (!right || 2 * f + 1 >= len - 1 - i || cur <= right))
+          break
+
+        if (
+          left < cur &&
+          (!right || 2 * f + 1 >= len - 1 - i || left < right)
+        ) {
+          ;[this.heap[f], this.heap[2 * f]] = [this.heap[2 * f], this.heap[f]]
+          f = f * 2
+        } else if (
+          right &&
+          2 * f + 1 < len - 1 - i &&
+          right < cur &&
+          right < left
+        ) {
+          ;[this.heap[f], this.heap[2 * f + 1]] = [
+            this.heap[2 * f + 1],
+            this.heap[f]
+          ]
+          f = f * 2 + 1
+        }
+      }
+    }
+  }
+  /**
+   * 总感觉有bug
+   *
+   * (≖͞_≖̥)
+   *
+   * 流下了没技术的眼泪
+   *
+   * ༼ ༎ຶ ෴ ༎ຶ༽
+   */
+}
+
+// 测试代码
+// const test = new MinHeap()
+// test.insert(55)
+// test.insert(30)
+// test.insert(50)
+// test.insert(44)
+// test.insert(35)
+// test.insert(5)
+// test.insert(3)
+// test.insert(10)
+// test.sort()
+
+/**
  * 最大堆
  * 2020/1/2
  */
@@ -28,35 +149,26 @@ class MaxHeap {
   //删除
   del(value) {
     let i = this.heap.indexOf(value)
-    if (i === -1) return
-
-    //使用最后一个元素替换当前元素
-    //并删除最后一个元素
-    this.heap[i] = this.heap[this.heap.length - 1]
+    if (i === -1) return //要删除的节点与最后一个节点交换位置
+    ;[this.heap[i], this.heap[this.heap.length - 1]] = [
+      this.heap[this.heap.length - 1],
+      this.heap[i]
+    ]
+    //删除最后一个节点
     this.heap.pop()
-    this.sortOne(i, this.heap.length)
-  }
 
-  sortOne(i, length) {
-    //循环条件为该节点左子树存在，且不为已排好序的元素
-    while (2 * i <= length) {
+    while (2 * i <= this.heap.length) {
       let cur = this.heap[i],
-        left = this.heap[i * 2],
-        right = this.heap[i * 2 + 1]
-      //当前节点大于左右子树，退出循环
-      if (
-        (cur > left && right !== undefined && cur > right) ||
-        (cur > left && right === undefined)
-      )
-        return
-      if (cur < left && (right === undefined || left > right)) {
-        //当前节点左子树最大，与左子树交换位置
-        ;[this.heap[i], this.heap[i * 2]] = [this.heap[i * 2], this.heap[i]]
+        right = this.heap[2 * i + 1],
+        left = this.heap[2 * i]
+
+      if (cur >= left && (!right || cur >= right)) return
+      if (left > cur && (!right || left > right)) {
+        ;[this.heap[i], this.heap[2 * i]] = [this.heap[2 * i], this.heap[i]]
         i = i * 2
-      } else if (right !== undefined && cur < right && right > left) {
-        //当前节点右子树最大，与右子树交换位置
-        ;[this.heap[i], this.heap[i * 2 + 1]] = [
-          this.heap[i * 2 + 1],
+      } else if (right && right > cur && right > left) {
+        ;[this.heap[i], this.heap[2 * i + 1]] = [
+          this.heap[2 * i + 1],
           this.heap[i]
         ]
         i = i * 2 + 1
@@ -66,32 +178,53 @@ class MaxHeap {
 
   //排序（升序）
   sort() {
+    if (this.heap.length < 3) return
+
     for (let i = 1, len = this.heap.length; i < len; i++) {
+      //第一个和倒数第i个数交换位置
       ;[this.heap[1], this.heap[len - i]] = [this.heap[len - i], this.heap[1]]
 
-      //不懂为啥要减2
-      //我的思路是减1
-      //但是试了要减2才行
-      this.sortOne(1, this.heap.length - i - 2)
+      //维持第i个数之前的所有数为一个大顶堆
+      let f = 1
+      while (2 * f <= len - 1 - i) {
+        let cur = this.heap[f],
+          left = this.heap[f * 2],
+          right = this.heap[f * 2 + 1]
 
-      /**
-       * 好懵逼啊
-       * 流下了没技术的眼泪
-       * ༼ಢ_ಢ༽
-       */
+        if (cur >= left && (!right || 2 * f + 1 >= len - 1 - i || cur >= right))
+          break
+
+        if (
+          left > cur &&
+          (!right || 2 * f + 1 >= len - 1 - i || left > right)
+        ) {
+          ;[this.heap[f], this.heap[2 * f]] = [this.heap[2 * f], this.heap[f]]
+          f = f * 2
+        } else if (
+          right &&
+          2 * f + 1 < len - 1 - i &&
+          right > cur &&
+          right > left
+        ) {
+          ;[this.heap[f], this.heap[2 * f + 1]] = [
+            this.heap[2 * f + 1],
+            this.heap[f]
+          ]
+          f = f * 2 + 1
+        }
+      }
     }
   }
 }
-/**
- * 测试代码
- * const test = new MaxHeap()
- * test.insert(55)
- * test.insert(30)
- * test.insert(50)
- * test.insert(44)
- * test.insert(35)
- * test.insert(5)
- * test.insert(3)
- * test.insert(10)
- * test.sort() //[null, 3, 5, 10, 30, 35, 44, 50, 55]
- */
+
+//  测试代码
+// const test = new MaxHeap()
+// test.insert(55)
+// test.insert(30)
+// test.insert(50)
+// test.insert(44)
+// test.insert(35)
+// test.insert(5)
+// test.insert(3)
+// test.insert(10)
+// test.sort()
